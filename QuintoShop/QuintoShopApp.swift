86dea0701fd1d0c9f295626c0290c865548af863
Denisov Ruslan <denisovitapi@gmail.com>
2023-06-04 15:07:18 +0200

    Initial Commit

---
+++ QuintoShop/QuintoShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuintoShopApp.swift
+//  QuintoShop
+//
+//  Created by Руслан on 04.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuintoShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
